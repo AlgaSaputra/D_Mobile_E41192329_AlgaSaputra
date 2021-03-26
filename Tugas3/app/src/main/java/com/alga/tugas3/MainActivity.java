0af9df2776e0d50e0939a875760588f0591b2dc6
Alga Saputra <68761934+AlgaSaputra@users.noreply.github.com>
2021-03-26 09:24:59 +0700

    Tugas3_AlgaSaputra_E41192329_D

---
+++ Tugas3/app/src/main/java/com/alga/tugas3/MainActivity.java
@@ -0,0 +1,20 @@
+package com.alga.tugas3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void actionToast1(View view) {
+        Toast.makeText(this,"Tombol berhasil", Toast.LENGTH_SHORT). show();
+    }
+}
